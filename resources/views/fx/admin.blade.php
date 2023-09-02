81f6548a0654a580286c0b4d6704838e0ea3b053
hayata19791218 <hayatasannhayatasann@gmail.com>
2023-09-02 22:12:02 +0900

    見た目の変更

---
+++ resources/views/fx/admin.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/reset.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/admin.css') }}" rel="stylesheet" type="text/css">
+    <title>管理画面</title>
+</head>
+<body>
+    <div class="container">
+        <h1>管理画面</h1>
+        @if(session('message'))
+        <p class="message">{{ session('message') }}</p>
+        @endif
+        <table>
+            <tr>
+                <th>日付</th>
+                <th>最高値</th>
+                <th>最安値</th>
+                <th></th>
+                <th></th>
+            </tr>
+            @foreach($editDatas as $editData)
+            <tr>
+                <td>{{ $editData->date }}</td>
+                <td>{{ $editData->high_value }}</td>
+                <td>{{ $editData->row_value }}</td>
+                <td><a href="{{ route('fx.edit', ['id' => $editData->id]) }}">編集</a></td>
+                <td>
+                    <form method="post" action="{{ route('fx.delete', ['value' => $editData]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button onClick="return confirm('本当に削除しますか？');">削除</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+        <a href="/" class="back">チャートのページに戻る</a>
+    </div>
+</body>
+</html>
+
+
