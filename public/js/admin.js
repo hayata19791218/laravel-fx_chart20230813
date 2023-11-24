d0d6c175bfe3f393a7fde1fe32df2c349687d3fa
hayata19791218 <hayatasannhayatasann@gmail.com>
2023-11-24 18:59:31 +0900

    見た目の修正

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,12 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*******************************!*\
-  !*** ./resources/js/admin.js ***!
-  \*******************************/
-var valueDelete = document.getElementById("delete");
-var valueRow = document.getElementById("row");
-valueDelete.addEventListener('click', function () {
-  if (confirm("情報を消していいですか？")) valueRow.remove();
-});
-/******/ })()
-;
+(()=>{var e=document.getElementById("delete"),t=document.getElementById("row");e.addEventListener("click",(function(){confirm("情報を消していいですか？")&&t.remove()}))})();
